8908615ca18ebb042a06446998ab9c82eddd9a2e
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-07 03:07:38 +0200

    themes + plugins tabs

--- src/core-plugins/marketplace/index.jsx
+++ src/core-plugins/marketplace/index.jsx
@@ -2,18 +2,16 @@ module.exports = () => {
   const React = r.a;
   require('../components');
   require('./data');
+  require('./plugins');
+  require('./themes');
 
   return (
     <>
       <div className="container">
         <h1 className="heading-1">Deezer Tweaker Marketplace</h1>
         <Tabs items={[
-          { name: 'Plugins', hideTitle: true, component: <DataComponent title="Plugins" data={[
-              { name: 'No Premium' }
-            ]} /> },
-          { name: 'Themes', hideTitle: true, component: <DataComponent title="Themes" data={[
-              { name: 'No Premium' }
-            ]} /> },
+          { name: 'Plugins', hideTitle: true, component: <PluginsComponent /> },
+          { name: 'Themes', hideTitle: true, component: <ThemesComponent /> },
           { name: 'Installed', hideTitle: true, component:
             <>
               <DataComponent title="Plugins" data={[

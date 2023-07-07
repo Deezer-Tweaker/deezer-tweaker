088a3ee9413001e4356a04c1e1dde3cdd954f81f
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-07 12:56:19 +0200

    custom css

--- src/core-plugins/options-page/index.jsx
+++ src/core-plugins/options-page/index.jsx
@@ -1,26 +1,32 @@
 module.exports = () => {
   const React = r.a;
+  const fs = require('fs');
+  const { join } = require('path');
+  const { paths } = window.DeezerTweaker;
   require('../components');
 
   const HomeComponent = () => {
+    const [customCss, setCustomCss] = React.useState(fs.readFileSync(join(paths.data, 'custom.css'), 'utf8'));
+
     return (
       <>
         <div>
-          <textarea className="chakra-textarea form-control form-control-block css-1f0xw6y e3mndjk0"></textarea>
+          <div className="container" style={{ padding: 0 }}>
+            <h2 className="heading-2">Custom CSS</h2>
+          </div>
+          <div className="container">
+            <textarea
+              className="chakra-textarea form-control form-control-block css-1f0xw6y e3mndjk0" rows="8"
+              onChange={(e) => setCustomCss(e.target.value)}
+            >{customCss}</textarea>
+            <div style={{ marginTop: 'var(--tempo-space-6)' }}>
+              <Button onClick={() => fs.writeFileSync(join(paths.data, 'custom.css'), customCss)}>Save</Button>
+            </div>
+          </div>
         </div>
       </>
     );
   }
-
-  const PluginsComponent = ({ data }) => {
-    return (
-      <>
-        <div>
-          <Table columns={['name', 'author', 'description', 'enabled']} rows={data} />
-        </div>
-      </>
-    );
-  };
 
   return (
     <div className="container">

be0f83cceefe6142811c85240fc3d7dbe518ba6a
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-08 14:41:36 +0200

    fixed path

--- src/core-plugins/marketplace/installed.jsx
+++ src/core-plugins/marketplace/installed.jsx
@@ -1,6 +1,6 @@
 const InstalledComponent = () => {
   const plugins = fs.readdirSync(join(paths.data, 'plugins')).map(f => {
-    return require(join(paths.data, 'plugins', f, 'manifest.json'));
+    return require(join(paths.data, 'plugins', f, `${f}.js`));
   });
   return (
     <>

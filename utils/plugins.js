1a3935e763095188e23189eea10a97af2418294a
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-08 21:13:19 +0200

    plugins helpers

--- utils/plugins.js
+++ utils/plugins.js
@@ -1,8 +1,52 @@
-const { readdirSync } = require('fs');
+const { readdirSync, existsSync, writeFileSync } = require('fs');
 const { join } = require('path');
 const paths = require('./paths');
+const fs = require('fs');
 
 module.exports.apply = (startup = false) => {
+  const DeezerTweaker = {
+    Api: {
+      Routes: {
+        create(path, component) {
+          require('../utils/asar').replaceInFile(
+            join(paths.extractedAsar, 'build', 'assets', 'cache', 'js', 'app-web.b8b99a13a697527a646c.js'),
+            /(,{exact:!0,path:"\/",redirectTo:`\/\$\{e}\/`})/g,
+            `$1,{ exact: true, path: '${path}', redirectTo: \`\\/\${e}${path}\` },{ exact: true, path: b('${path}'), component: ${component?.toString()?.replace('React', 'r.a')} }`
+          );
+        },
+        redirect(path, to) {
+          require('../utils/asar').replaceInFile(
+            join(paths.extractedAsar, 'build', 'assets', 'cache', 'js', 'app-web.b8b99a13a697527a646c.js'),
+            /(,{exact:!0,path:"\/",redirectTo:`\/\$\{e}\/`})/g,
+            `$1,{ exact: true, path: '${path}', redirectTo: '${to}' }`
+          );
+        }
+      }
+    },
+    Plugins: (() => {
+      let plugins = [];
+      fs.readdirSync(join(paths.data, 'plugins')).map(f => require(join(paths.data, 'plugins', f, `${f}.js`))).forEach(plugin => {
+        Object.assign(plugin, {
+          Settings: {
+            file: join(paths.data, 'settings', `${plugin.name}.json`),
+            set(key, value) {
+              if (!existsSync(this.file)) writeFileSync(this.file, '{}');
+              const settings = require(this.file);
+              settings[key] = value;
+              writeFileSync(this.file, JSON.stringify(settings));
+            },
+            get(key) {
+              if (!existsSync(this.file)) return;
+              return key ? require(this.file)[key] : require(this.file);
+            }
+          }
+        });
+        plugins[plugin.name] = plugin;
+      });
+      return plugins;
+    })(),
+  };
+  if (startup) Object.assign(window.DeezerTweaker, DeezerTweaker);
   readdirSync(join(paths.data, 'plugins')).forEach(f => {
     const plugin = require(join(paths.data, 'plugins', f, `${f}.js`));
     const jsCachePath = join(paths.extractedAsar, 'build', 'assets', 'cache', 'js');
@@ -16,27 +60,7 @@ module.exports.apply = (startup = false) => {
     }
     if (plugin.inject && typeof plugin.inject === 'function') {
       if (startup) return;
-      plugin.inject({
-        React: require('react'),
-        Api: {
-          Routes: {
-            create(path, component) {
-              require('../utils/asar').replaceInFile(
-                join(paths.extractedAsar, 'build', 'assets', 'cache', 'js', 'app-web.b8b99a13a697527a646c.js'),
-                /(,{exact:!0,path:"\/",redirectTo:`\/\$\{e}\/`})/g,
-                `$1,{ exact: true, path: '${path}', redirectTo: \`\\/\${e}${path}\` },{ exact: true, path: b('${path}'), component: ${component?.toString()?.replace('React', 'r.a')} }`
-              );
-            },
-            redirect(path, to) {
-              require('../utils/asar').replaceInFile(
-                join(paths.extractedAsar, 'build', 'assets', 'cache', 'js', 'app-web.b8b99a13a697527a646c.js'),
-                /(,{exact:!0,path:"\/",redirectTo:`\/\$\{e}\/`})/g,
-                `$1,{ exact: true, path: '${path}', redirectTo: '${to}' }`
-              );
-            }
-          }
-        }
-      });
+      plugin.inject(DeezerTweaker);
     }
   });
 };

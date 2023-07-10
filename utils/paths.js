af2cf4d14360e09e5de27d281ec6cb08203e2b66
JustYuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-10 14:31:52 +0200

    paths: added support for macos

--- utils/paths.js
+++ utils/paths.js
@@ -1,10 +1,21 @@
 const { join } = require('path');
 
-module.exports = {
-  asar: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar'),
-  asarBackup: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar.bkp'),
-  extractedAsar: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app-extracted'),
-  program: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop'),
-  data: join(process.env.LOCALAPPDATA, 'deezer-tweaker'),
-  corePlugins: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar', 'dtjs', 'core-plugins')
-};
+if (process.platform === 'win32') {
+  module.exports = {
+    asar: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar'),
+    asarBackup: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar.bkp'),
+    extractedAsar: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app-extracted'),
+    program: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop'),
+    data: join(process.env.LOCALAPPDATA, 'deezer-tweaker'),
+    corePlugins: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar', 'dtjs', 'core-plugins')
+  };
+} else if (process.platform === 'darwin') {
+  module.exports = {
+    asar: join('/Applications', 'Deezer.app', 'Contents', 'Resources', 'app.asar'),
+    asarBackup: join('/Applications', 'Deezer.app', 'Contents', 'Resources', 'app.asar.bkp'),
+    extractedAsar: join('/Applications', 'Deezer.app', 'Contents', 'Resources', 'app-extracted'),
+    program: join('/Applications', 'Deezer.app'),
+    data: join(require('os').homedir(), 'Library', 'Application Support', 'deezer-tweaker'),
+    corePlugins: join('/Applications', 'Deezer.app', 'Contents', 'Resources', 'app-extracted', 'dtjs', 'core-plugins')
+  };
+}

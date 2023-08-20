e45582d1a8201bf07e3cf2b7b503c0ff9c95de21
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-20 15:00:00 +0200

    paths

--- utils/paths.js
+++ utils/paths.js
@@ -1,21 +1,39 @@
 const { join } = require('path');
+const fs = require('fs');
 
-if (process.platform === 'win32') {
-  module.exports = {
+const paths = {
+  win32: {
     asar: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar'),
     asarBackup: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar.bkp'),
     extractedAsar: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app-extracted'),
     program: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop'),
     data: join(process.env.LOCALAPPDATA, 'deezer-tweaker'),
     corePlugins: join(process.env.LOCALAPPDATA, 'Programs', 'deezer-desktop', 'resources', 'app.asar', 'dtjs', 'core-plugins')
-  };
-} else if (process.platform === 'darwin') {
-  module.exports = {
+  },
+  darwin: {
     asar: join('/Applications', 'Deezer.app', 'Contents', 'Resources', 'app.asar'),
     asarBackup: join('/Applications', 'Deezer.app', 'Contents', 'Resources', 'app.asar.bkp'),
     extractedAsar: join('/Applications', 'Deezer.app', 'Contents', 'Resources', 'app-extracted'),
     program: join('/Applications', 'Deezer.app'),
     data: join(require('os').homedir(), 'Library', 'Application Support', 'deezer-tweaker'),
     corePlugins: join('/Applications', 'Deezer.app', 'Contents', 'Resources', 'app-extracted', 'dtjs', 'core-plugins')
-  };
-}
+  }
+};
+
+/**
+ * Finds a build file in the specified directory
+ * @param {string} file
+ * @param {{
+ *   dirPath: string
+ * }} [options]
+ */
+const findFile = (file, options = {}) => {
+  if (file.split('.').length > 1) file = file.split('.')[0];
+  if (!options) options = {};
+  if (!options.dirPath) options.dirPath = join('assets', 'cache', 'js');
+  const files = fs.readdirSync(join(paths[process.platform].extractedAsar, 'build', options.dirPath));
+  return files.find(f => f.startsWith(file));
+};
+
+module.exports.findFile = findFile;
+module.exports = paths[process.platform];

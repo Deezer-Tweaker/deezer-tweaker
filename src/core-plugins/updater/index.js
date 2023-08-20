5e85de70b42d989f27ba28ffe6f54e752f3a0399
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-20 16:34:24 +0200

    updater

--- src/core-plugins/updater/index.js
+++ src/core-plugins/updater/index.js
@@ -1,9 +1,10 @@
-const {
-  join
-} = require('path');
+const { join } = require('path');
+const { existsSync, writeFileSync } = require('fs');
+
 const updateUrl = 'https://api.github.com/repos/Deezer-Tweaker/deezer-tweaker/releases/latest';
 fetch(updateUrl).then(res => res.json()).then(json => {
   if (json.tag_name !== window.DeezerTweaker.version) {
+    if (!existsSync(join(paths.data, 'settings.json'))) writeFileSync(join(paths.data, 'settings.json'), '{}');
     if (require(join(paths.data, 'settings.json')).notification_if_update_available) {
       const notification = new Notification('New update available!', {
         body: `The version ${json.tag_name} is available to download! Click on this notification to download it.`

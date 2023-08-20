07e9c63f58f0ccfbf67d509b26a2bc06dde2012f
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-20 16:34:39 +0200

    plugins

--- utils/plugins.js
+++ utils/plugins.js
@@ -108,10 +108,9 @@ module.exports.apply = (startup = false) => {
   if (startup) Object.assign(window.DeezerTweaker, DeezerTweaker);
   readdirSync(join(paths.data, 'plugins')).forEach(f => {
     const plugin = require(join(paths.data, 'plugins', f, `${f}.js`));
-    const jsCachePath = join(paths.extractedAsar, 'build', 'assets', 'cache', 'js');
     if (plugin.replacements && plugin.replacements.length !== 0) plugin.replacements.forEach(replacement => {
       if (!replacement.applyAtStartup && startup) return;
-      replaceInFile(replacement.file.replace('%jsCache%', jsCachePath), replacement.find, replacement.replace);
+      replaceInFile(findFile(replacement.file.replace('%jsCache%/', '')), replacement.find, replacement.replace);
     });
     if (plugin.css && typeof plugin.css === 'string' && plugin.css.trim() !== '') {
       if (startup) return;

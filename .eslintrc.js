b6ed0581197847c79b4f34ebcd17b800b57de142
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-09-01 14:38:12 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,47 @@
+/*
+ * Deezer Tweaker, a tool to mod Deezer desktop app!
+ * Copyright (c) 2023 Yuuto
+ * Licensed under the MPL-2.0 license
+ */
+
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-  'extends': [
-    'eslint:recommended'
-  ],
-  'overrides': [
+  extends: ['eslint:recommended'],
+  overrides: [
     {
-      'env': {
-        'node': true
+      env: {
+        node: true
       },
-      'files': [
-        '.eslintrc.{js,cjs}'
-      ],
-      'parserOptions': {
-        'sourceType': 'script'
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
       }
     }
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  'plugins': [],
-  'rules': {
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'object-curly-spacing': ['error', 'always']
+  plugins: ['simple-header'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'quote-props': ['error', 'as-needed'],
+    'no-unused-vars': ['error'],
+    'spaced-comment': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
+    'simple-header/header': ['error', {
+      text: [
+        'Deezer Tweaker, a tool to mod Deezer desktop app!',
+        'Copyright (c) {year} {author}',
+        'Licensed under the MPL-2.0 license',
+      ],
+      templates: { author: ['.*', 'Yuuto'] },
+    }]
   }
 };

39388e1ea995d1117b99740a4a5f213aae8eb499
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-09-04 15:14:17 +0200

    FreePremium plugin

--- src/plugins/FreePremium.js
+++ src/plugins/FreePremium.js
@@ -37,3 +37,35 @@ replaceInFile(
 
   replaceInFile(file, /(if\([a-zA-Z]!==[a-zA-Z]\)return [a-zA-Z]&&)![a-zA-Z](\?void be.[a-zA-Z].push\(Object\()/g, '$1false$2');
 });
+
+// Remove limited mode
+replaceInFile(findFile('app-web'), /[a-zA-Z]{1,2}.user_status.limited/g, 'false');
+
+// Set offer ID
+replaceInFile(findFile('legacy'), /([a-zA-Z].user.offerId|[a-zA-Z].OFFER_ID)/g, '(600)');
+replaceInFile(findFile('route-naboo'), /(userOfferId:)(Object\([a-zA-Z].[a-zA-Z]{1,2}\)\([a-zA-Z]\)|[a-zA-Z][^},])/g, '$1600');
+replaceInFile(findFile('route-naboo'), /(getOfferId:function\(\)\{return )Object\(z.z\)\(g.a.getState\(\).user\)(})/g, '$1600$2');
+
+// Remove upgrade buttons
+replaceInFile(findFile('legacy'), /Object\(r.a\)\([a-zA-Z],"USER.ENTRYPOINTS",""\)/g, '{}');
+replaceInFile(findFile('route-naboo'), /(entryPoints:)Object\(A.l\)\([a-zA-Z].user\)/g, '$1{}');
+
+// Enable downloading songs
+replaceInFile(findFile('app-web'), /this.props.hasDownload/, 'true');
+replaceInFile(findFile('app-web'), /const(\{([a-zA-Z:,]+)?userHasDownload:([a-zA-Z])([a-zA-Z:,]+)?}=[a-zA-Z]),/g, 'let $1;$3=true;const ');
+
+['app-web', 'route-naboo'].forEach(file => {
+  replaceInFile(findFile(file), /(userHasDownload):Object\([a-zA-Z].[a-zA-Z]\)\([a-zA-Z]\)/g, '$1:true');
+});
+['route-playlist', 'route-album', 'route-show', 'route-episode', 'route-track'].forEach(file => {
+  replaceInFile(findFile(file), /const(\{([a-zA-Z:,]+)?userHasDownload:([a-zA-Z])([a-zA-Z:,]+)?}=this.props);/g, 'let $1;$3=true;');
+  replaceInFile(findFile(file), /(const\{([a-zA-Z:,]+)?isDownloadable:[a-zA-Z]([a-zA-Z:,]+)?}=[a-zA-Z].[a-zA-Z].useContext\([a-zA-Z].[a-zA-Z]\);return )[a-zA-Z]/g, '$1true');
+
+  replaceInFile(findFile(file), /(userHasDownload):Object\([a-zA-Z].[a-zA-Z]\)\([a-zA-Z]\)/g, '$1:true');
+  replaceInFile(findFile(file), /(isDownloadable):(![0-9]|Object\([a-zA-Z].[a-zA-Z]\)\([a-zA-Z]\))/g, '$1:true');
+});
+replaceInFile(
+  findFile('route-profile'),
+  /(_getTabList\(\)\{const{[a-zA-Z:,]+}=this.props,[a-zA-Z]=[a-zA-Z]{1,2}\([a-zA-Z],)[a-zA-Z]/g,
+  '$1true'
+);

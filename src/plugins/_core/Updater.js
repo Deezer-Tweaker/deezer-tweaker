98ced8f908b2e6429d10f898c37dc1d3d8b17671
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-09-06 17:02:03 +0200

    fix: updater

--- src/plugins/_core/Updater.js
+++ src/plugins/_core/Updater.js
@@ -4,22 +4,37 @@
  * Licensed under the MPL-2.0 license
  */
 
-/* global electron, join */
+/* global DeezerTweaker, electron */
 
-const { existsSync, writeFileSync } = require('fs');
-const paths = require('../../../utils/paths');
-
-const updateUrl = 'https://api.github.com/repos/Deezer-Tweaker/deezer-tweaker/releases/latest';
-fetch(updateUrl).then(res => res.json()).then(json => {
-  if (json.tag_name !== window.DeezerTweaker.version) {
-    if (!existsSync(join(paths.data, 'settings.json'))) writeFileSync(join(paths.data, 'settings.json'), '{}');
-    if (require(join(paths.data, 'settings.json')).notification_if_update_available) {
-      const notification = new Notification('New update available!', {
-        body: `The version ${json.tag_name} is available to download! Click on this notification to download it.`
-      });
-      notification.addEventListener('click', () => {
-        electron.openExternalLink(json.assets[0].browser_download_url);
-      });
+setTimeout(() => {
+  const updateUrl = 'https://api.github.com/repos/Deezer-Tweaker/deezer-tweaker/releases/latest';
+  fetch(updateUrl).then(res => res.json()).then(json => {
+    if (json.tag_name !== window.DeezerTweaker.version) {
+      const React = DeezerTweaker.importWebpackModule.n(DeezerTweaker.importWebpackModule('q1tI')).a;
+      const ReactDOM = DeezerTweaker.importWebpackModule.n(DeezerTweaker.importWebpackModule('i8i4')).a;
+      const Popper = DeezerTweaker.importWebpackModule('hG0x').a;
+      const Button = DeezerTweaker.importWebpackModule('s86Q').a;
+      const data = {
+        colorScheme: 'gray',
+        message: `The version ${json.tag_name} of Deezer Tweaker is available to download!`,
+        action: [React.createElement(Button, {
+          colorScheme: 'white', variant: 'outline', onClick: () => electron.openExternalLink(json.assets[0].browser_download_url),
+          key: 'download'
+        }, 'Download')]
+      };
+      const render = React.createElement(Popper, data);
+      const alert = React.createElement('div', { className: 'alert-wrapper' }, render);
+      ReactDOM.createRoot(document.querySelector('.page-alerts')).render(alert);
+      const i = setInterval(() => {
+        const alert = document.querySelector('.css-3mqnm2.chakra-alert[role="alert"]');
+        if (alert) clearInterval(i); else return;
+        alert.querySelector('.chakra-alert__title').className = 'chakra-alert__title css-4j2z69 e3mndjk0';
+        alert.querySelector('.chakra-alert__desc').className = 'chakra-alert__desc css-vfhx9v e3mndjk0';
+        alert.className = 'chakra-alert e3mndjk0 css-1eou3g4';
+        alert.querySelector('.chakra-stack').addEventListener('click', () => {
+          alert.parentElement.remove();
+        });
+      }, 1);
     }
-  }
-});
+  });
+}, 6000);
